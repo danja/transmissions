0ba647df8b2978c57b2b8fbd607b0b1868d61b33
Danny Ayers <danny.ayers@gmail.com>
2024-05-01 21:02:44 +0200

    brain given up

---
+++ raw-src/structure-play.js
@@ -0,0 +1,14 @@
+// let root = {}
+// let a = { b: { c: 'C' } }
+let b = 'B'
+let a = 'twat'
+
+
+let c = a => b
+
+
+//root.a = a
+// console.log(root.a)
+
+//let root.a.b = 'B'
+// console.log(root.a.b)
