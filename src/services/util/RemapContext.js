faaab936d46405d5a34162b9bb38d20346c20e29
Danny Ayers <danny.ayers@gmail.com>
2024-07-08 13:16:41 +0200

    pre-rename context to message

--- src/services/util/RemapContext.js
+++ src/services/util/RemapContext.js
@@ -1,33 +1,46 @@
 import logger from '../../utils/Logger.js'
 import rdf from 'rdf-ext'
-import grapoi from 'grapoi'
-import { fromFile, toFile } from 'rdf-utils-fs'
 
 import ns from '../../utils/ns.js'
 import GrapoiHelpers from '../../utils/GrapoiHelpers.js'
 import Service from '../base/Service.js'
 
+/**
+ * Changes the shape of the context object by renaming keys.
+ * 
+ * #### __*Input*__
+ * **context** : ?
+ * #### __*Output*__
+ * **context** : ?
+ * 
+ * @extends Service
+ */
 class RemapContext extends Service {
+    /**
+     * Creates an instance of RemapContext.
+     * @param {Object} config - The configuration object.
+     */
     constructor(config) {
         super(config)
     }
 
+    /**
+     * Executes the remapping of the context object.
+     * @param {Object} context - The context object to be remapped.
+     */
     async execute(context) {
         if (this.preProcess(context)) {
             return
         }
 
-        //    logger.log('SERVICE this.configKey = ' + this.configKey.value)
+        logger.debug('RemapContext this.configKey = ' + this.configKey.value)
 
         const renames = GrapoiHelpers.listToArray(this.config, this.configKey, ns.trm.rename)
         const dataset = this.config
-        //  logger.log('renames  = ' + renames)
-        // for (const rename in renames) {
+
         for (let i = 0; i < renames.length; i++) {
             let rename = renames[i]
-
             let poi = rdf.grapoi({ dataset: dataset, term: rename })
-
             let pre = poi.out(ns.trm.pre).value
             let post = poi.out(ns.trm.post).value
             var value
@@ -36,26 +49,26 @@ class RemapContext extends Service {
             // for copying value of eg. x.y.z to context.b
             if (pre.includes('.')) {
                 const spre = pre.split('.')
-                logger.log('spre ' + spre)
+                logger.debug('pre- split = ' + spre)
                 value = context[spre[0]][spre[1]]
             } else {
                 value = context[pre]
             }
+
             // not properly tested, I didn't end up needing it
             value = value.toString()  // otherwise passes a Buffer
             // for copying value of eg. context.content to context.contentBlocks.content 
             if (post.includes('.')) {
                 const s = post.split('.')
-                //logger.log('sss ' + s)
+                logger.debug('post split = ' + s)
                 context[s[0]][s[1]] = value
             } else {
                 context[post] = value
             }
             logger.log(' - Rename : ' + pre + ' to ' + post)
         }
-        // process.exit()
         this.emit('message', context)
     }
 }
-// new name test
+
 export default RemapContext
