ee1a2fba2328980133977ed168a294121a15ee88
Danny Ayers <danny.ayers@gmail.com>
2024-08-01 14:39:38 +0200

    working!

--- src/services/fs/FileCopy.js
+++ src/services/fs/FileCopy.js
@@ -1,102 +1,85 @@
 import { copyFile, mkdir, readdir, stat } from 'node:fs/promises'
 import path from 'path'
-
 import logger from '../../utils/Logger.js'
 import ns from '../../utils/ns.js'
-import footpath from '../../utils/footpath.js'
-
 import Service from '../base/Service.js'
 
-/**
- * FileCopy class that extends Service.
- * Copies files or directories to a specified destination.
- */
 class FileCopy extends Service {
-
-    /**
-     * Constructs a new FileCopy instance.
-     * @param {Object} config - The configuration object.
-     */
     constructor(config) {
         super(config)
     }
 
-    /**
-     * Copies the source to the destination.
-     * @param {Object} message - The message object.
-     * @param {string} message.source - The source path (ignored if there's a configKey from transmission)
-     * @param {string} message.destination - The destination path  (ignored if there's a configKey from transmission)
-     */
     async execute(message) {
-        //  try {
         var source, destination
 
         if (this.configKey === 'undefined') {
-            logger.debug('FileCopy no configKey from transmission, using message.source, message.destination')
+            logger.debug('FileCopy: Using message.source and message.destination')
             source = message.source
             destination = message.destination
         } else {
-            logger.debug('FileCopy this.configKey = ' + this.configKey.value)
+            logger.debug(`FileCopy: Using configKey ${this.configKey.value}`)
             source = this.getPropertyFromMyConfig(ns.trm.source)
             destination = this.getPropertyFromMyConfig(ns.trm.destination)
-
             source = path.join(message.applicationRootDir, source)
             destination = path.join(message.applicationRootDir, destination)
         }
 
-        logger.log('\nsource = ' + source)
-        logger.log('destination = ' + destination + '\n')
-        // this.showMyConfig()
-
-        const sourceStat = await stat(source)
-
-        // const destPath = path.join(destination, source.split('/').pop())
+        logger.debug(`Source: ${source}`)
+        logger.debug(`Destination: ${destination}`)
 
         try {
+            await this.ensureDirectoryExists(path.dirname(destination))
+            const sourceStat = await stat(source)
+
             if (sourceStat.isFile()) {
-                console.log(`\nCopying file from ${source} to ${destination}\n`);
-                await copyFile(source, destination);
+                logger.debug(`Copying file from ${source} to ${destination}`)
+                await copyFile(source, destination)
             } else if (sourceStat.isDirectory()) {
-                console.log(`Copying directory from ${source} to ${destination}`);
-                await this.copyDirectory(source, destination);
+                logger.debug(`Copying directory from ${source} to ${destination}`)
+                await this.copyDirectory(source, destination)
             }
         } catch (err) {
-            console.error(`Error in FileCopy: ${err.message}`);
-            console.error(`Source: ${source}`);
-            console.error(`Destination: ${destination}`);
+            logger.error(`Error in FileCopy: ${err.message}`)
+            logger.error(`Source: ${source}`)
+            logger.error(`Destination: ${destination}`)
         }
 
         this.emit('message', message)
     }
 
-
-
-    /**
-     * Recursively copies a directory and its contents to the destination directory.
-     * @param {string} source - The source directory path.
-     * @param {string} destination - The destination directory path.
-     */
-    async copyDirectory(source, destination) {
+    async ensureDirectoryExists(dirPath) {
+        logger.debug(`Ensuring directory exists: ${dirPath}`)
         try {
-            const destDir = path.join(destination, source.split('/').pop())
-            await mkdir(destDir, { recursive: true })
+            await mkdir(dirPath, { recursive: true })
+            logger.debug(`Directory created/ensured: ${dirPath}`)
+        } catch (err) {
+            logger.debug(`Error creating directory ${dirPath}: ${err.message}`)
+            throw err
+        }
+    }
+
+    async copyDirectory(source, destination) {
+        logger.debug(`Copying directory: ${source} to ${destination}`)
+        try {
+            await this.ensureDirectoryExists(destination)
             const entries = await readdir(source, { withFileTypes: true })
 
             for (const entry of entries) {
                 const srcPath = path.join(source, entry.name)
-                const destPath = path.join(destDir, entry.name)
+                const destPath = path.join(destination, entry.name)
 
-                //   logger.log('srcPath = ' + srcPath)
-                //   logger.log('destPath = ' + destPath)
+                logger.debug(`Processing: ${srcPath} to ${destPath}`)
 
                 if (entry.isDirectory()) {
                     await this.copyDirectory(srcPath, destPath)
                 } else {
                     await copyFile(srcPath, destPath)
+                    logger.debug(`File copied: ${srcPath} to ${destPath}`)
                 }
             }
         } catch (err) {
-            logger.error("FileCopy.copyDirectory error: " + err.message)
+            logger.debug(`Error in copyDirectory: ${err.message}`)
+            throw err
         }
     }
 }
