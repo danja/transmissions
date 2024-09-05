d8ad4a7738e5d90a84e28798ca0aff52b5c4d03b
Danny Ayers <danny.ayers@gmail.com>
2024-09-05 18:28:26 +0200

    fixing command args, refactoring

--- run.js
+++ run.js
@@ -1,128 +1,26 @@
-// run.js - ES module style
+// CommandUtils.js - run.js helpers
+
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 // import { promises as fs } from 'fs';
 import path from 'path'
 import fs from 'fs/promises'
-
-import TransmissionBuilder from './src/engine/TransmissionBuilder.js'
 import { fileURLToPath } from 'url';
 
-import logger from './src/utils/Logger.js'
 
-var applicationsDir = './src/applications'
+//import TransmissionBuilder from './src/engine/TransmissionBuilder.js'
+
+
+import logger from './src/utils/Logger.js'
+import CommandUtils from './src/utils/CommandUtils.js'
+
+var applicationsDir = './src/applications' // default inside transmissions
 
 
 
 // logger.log('DESCRIBE ' + await transmission.describe())
 
-class CommandUtils {
 
-    // TODO refactor all this out
-    static async run(appsDir, application, message = {}) {
-
-
-        logger.debug('\nRUN, appsDir =' + appsDir)
-        logger.debug('RUN, application =' + application)
-
-        const appSplit = CommandUtils.splitName(application)
-
-        var appName = appSplit.first
-        logger.debug(' = ' + appName)
-        //     if (appsDir) {
-        //       dir = appsDir // path.join(appsDir, dir)
-        // }
-        var dir = path.join(appsDir, appName)
-        ///  path.join(appsDir, appName)
-        logger.debug('DIR = ' + dir)
-        const subtask = appSplit.second
-
-        // message.dataString = data // TODO tidy/remove
-        //    message.rootApplication = application
-
-        logger.debug("run.js, Hello, logger!")
-        logger.debug("process.cwd() = " + process.cwd())
-
-        logger.debug("dir = " + dir)
-        var transmissionConfigFile = path.join(dir, 'transmissions.ttl')
-
-
-        // TODO remove once files renamed
-        //   try {
-        //     await fs.access(transmissionConfigFile);
-        //} catch (error) {
-        //  transmissionConfigFile = path.join(dir, 'transmission.ttl')
-        //}
-        ////////////
-
-        const servicesConfigFile = path.join(dir, 'services.ttl')
-
-        const transmissions = await TransmissionBuilder.build(transmissionConfigFile, servicesConfigFile)
-
-        if (subtask) {
-            for (var i = 0; i < transmissions.length; i++) {
-                if (subtask === transmissions[i].label) {
-                    transmissions[i].execute(message)
-                }
-            }
-        }
-        else {
-            for (var i = 0; i < transmissions.length; i++) {
-                transmissions[i].execute(message)
-            }
-        }
-    }
-
-    static async parseOrLoadContext(contextArg) {
-        let message = ''
-        try {
-            message = JSON.parse(contextArg)
-        } catch (err) {
-            logger.log(err)
-            // If it fails, assume it's a filename and try to load the file
-            const filePath = path.resolve(contextArg); // Ensure the path is absolute
-            const fileContent = await fs.readFile(filePath, 'utf8');
-            message = JSON.parse(fileContent)
-            logger.log('message from file = ' + message)
-        }
-        return message
-
-    }
-
-    static async listSubdirectories(currentDirectory) {
-        try {
-            const entries = await fs.readdir(currentDirectory, { withFileTypes: true });
-
-            const subdirChecks = entries.filter(dirent => dirent.isDirectory()).map(async (dirent) => {
-                const subdirPath = path.join(currentDirectory, dirent.name);
-                const files = await fs.readdir(subdirPath);
-                return files.includes('about.md') ? dirent.name : null;
-            })
-
-            // Resolve all promises from subdirChecks and filter out null values
-            const subdirsContainingAboutMd = (await Promise.all(subdirChecks)).filter(name => name !== null);
-
-            subdirsContainingAboutMd.forEach(dir => console.log(dir));
-        } catch (err) {
-            console.error('Error listing subdirectories:', err);
-        }
-    }
-
-    static splitName(input) {
-        if (input.includes('.')) {
-            let parts = input.split('.');
-            return {
-                first: parts[0],
-                second: parts[1]
-            };
-        } else {
-            return {
-                first: input,
-                second: false
-            };
-        }
-    }
-}
 
 let message = {}
 
@@ -161,81 +59,17 @@ if (process.argv.length <= 2) {
                 })
         }, async (argv) => {
             const { dir, application, target, message: contextArg } = argv
+            if (dir) {
+                applicationsDir = dir
+            }
             logger.setLogLevel("info")
-            logger.debug('dir = ' + dir)
+            logger.debug('\n**** run.js, async (args)')
+            logger.debug('applicationsDir = ' + applicationsDir)
             logger.debug('application = ' + application)
             logger.debug('target = ' + target)
             // logger.reveal('message = ' + message)
 
-            // process.exit()
-
-            // TODO this is a bloody mess. Refactor!
-            const appSplit = CommandUtils.splitName(application)
-            //  var dir = appSplit.first
-
-            logger.debug('appSplit.first = ' + appSplit.first)
-            //  if (dir) {
-            //    applicationsDir = path.join(dir, appSplit.first) // TODO refactor
-            // }
-            logger.debug('appsDir = ' + applicationsDir)
-
-            const transmissionPath = path.join(applicationsDir, application)
-            const defaultDataDir = path.join(transmissionPath, '/data')
-
-            //      applicationsDir, application
-            logger.debug('\nA PRERUN  applicationsDir =' + applicationsDir)
-            logger.debug('\nA PRERUN  application =' + application)
-            logger.debug('\n')
-            // TODO revisit base message, add constructor.name?
-            message = { "dataDir": defaultDataDir }
-            message.rootDir = target // application
-
-            // If a message argument was provided, parse or load it
-            if (contextArg) {
-                message = await CommandUtils.parseOrLoadContext(contextArg);
-            }
-
-            if (dir) { // TODO refactor with above
-                message.applicationRootDir = applicationsDir
-            } else {
-                message.applicationRootDir = path.join(fileURLToPath(import.meta.url), '../', transmissionPath)
-            }
-
-            logger.debug('transmissionPath = ' + transmissionPath)
-            //    process.exit()
-
-            // Claude gave me this madness
-            if (application === 'postcraft-init') {
-                // Read the services.ttl file
-                const servicesConfigFile = path.join(transmissionPath, 'services.ttl')
-                let servicesContent = await fs.readFile(servicesConfigFile, 'utf8')
-
-                // Replace the placeholder with the actual destination path
-                servicesContent = servicesContent.replace('{{destinationPath}}', application)
-
-                // Write the modified services.ttl back to a temporary file
-                const tempServicesFile = path.join(transmissionPath, 'temp_services.ttl')
-                await fs.writeFile(tempServicesFile, servicesContent)
-
-                // Build and execute the transmission with the temporary services file
-                const transmissionConfigFile = path.join(transmissionPath, 'transmission.ttl')
-                const transmissions = await TransmissionBuilder.build(transmissionConfigFile, tempServicesFile)
-                for (var i = 0; i < transmissions.length; i++) {
-                    await transmissions[i].execute({
-                        ...message,
-                        source: "/home/danny/HKMS/postcraft/postcraft-template/", // TODO why is this hardcoded!?
-                        destination: application,
-                        rootDir: application
-                    })
-                }
-
-                // Clean up the temporary file
-                await fs.unlink(tempServicesFile)
-            } else {
-                await CommandUtils.run(applicationsDir, application, message)
-
-                // run(appsDir, application, data, message = {}) {
-            }
+            await CommandUtils.run(applicationsDir, application, target, message)
         })
         .help('h')
         .alias('h', 'help')
---
+++ src/utils/CommandUtils.js
@@ -0,0 +1,123 @@
+// run.js - ES module style
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+// import { promises as fs } from 'fs';
+import path from 'path'
+import fs from 'fs/promises'
+
+import TransmissionBuilder from '../engine/TransmissionBuilder.js'
+import { fileURLToPath } from 'url';
+
+import logger from './Logger.js'
+
+var applicationsDir = '../applications'
+
+const defaultTransmissionsFilename = 'transmissions.ttl'
+const defaultServicesConfigFile = 'services-config.ttl'
+
+class CommandUtils {
+
+    // TODO refactor all this out
+    static async run(appsDir, application, target, message = {}) {
+        logger.setLogLevel('debug')
+        //  logger.debug("run.js, Hello, logger!")
+        // logger.debug("process.cwd() = " + process.cwd())
+        logger.debug('\nCommandUtils.run()')
+
+        logger.debug('CommandUtils, appsDir =' + appsDir)
+        logger.debug('CommandUtils, application =' + application)
+        logger.debug('CommandUtils, target =' + target)
+
+        const appSplit = CommandUtils.splitName(application)
+        const appName = appSplit.first
+        const subtask = appSplit.second
+
+        logger.debug('appName = ' + appName)
+        logger.debug('subtask  = ' + subtask)
+
+        var transmissionsDir = path.join(appsDir, appName)
+
+        logger.debug('transmissionsDir = ' + transmissionsDir)
+
+        const transmissionsFile = path.join(transmissionsDir, defaultTransmissionsFilename)
+        const servicesConfigFile = path.join(transmissionsDir, defaultServicesConfigFile)
+
+        logger.debug("transmissionConfigFile = " + transmissionsFile)
+        logger.debug("servicesConfigFile = " + servicesConfigFile)
+
+
+
+        const defaultDataDir = path.join(transmissionsDir, '/data')
+        message = { "dataDir": defaultDataDir }
+        message.rootDir = target // application target dir
+
+        // process.exit()
+
+        const transmissions = await TransmissionBuilder.build(transmissionsFile, servicesConfigFile)
+
+        if (subtask) {
+            for (var i = 0; i < transmissions.length; i++) {
+                if (subtask === transmissions[i].label) {
+                    transmissions[i].execute(message)
+                }
+            }
+        }
+        else {
+            for (var i = 0; i < transmissions.length; i++) {
+                transmissions[i].execute(message)
+            }
+        }
+    }
+
+    static async parseOrLoadContext(contextArg) {
+        let message = ''
+        try {
+            message = JSON.parse(contextArg)
+        } catch (err) {
+            logger.log(err)
+            // If it fails, assume it's a filename and try to load the file
+            const filePath = path.resolve(contextArg); // Ensure the path is absolute
+            const fileContent = await fs.readFile(filePath, 'utf8');
+            message = JSON.parse(fileContent)
+            logger.log('message from file = ' + message)
+        }
+        return message
+
+    }
+
+    static async listSubdirectories(currentDirectory) {
+        try {
+            const entries = await fs.readdir(currentDirectory, { withFileTypes: true });
+
+            const subdirChecks = entries.filter(dirent => dirent.isDirectory()).map(async (dirent) => {
+                const subdirPath = path.join(currentDirectory, dirent.name);
+                const files = await fs.readdir(subdirPath);
+                return files.includes('about.md') ? dirent.name : null;
+            })
+
+            // Resolve all promises from subdirChecks and filter out null values
+            const subdirsContainingAboutMd = (await Promise.all(subdirChecks)).filter(name => name !== null);
+
+            subdirsContainingAboutMd.forEach(dir => console.log(dir));
+        } catch (err) {
+            console.error('Error listing subdirectories:', err);
+        }
+    }
+
+    static splitName(input) {
+        if (input.includes('.')) {
+            let parts = input.split('.');
+            return {
+                first: parts[0],
+                second: parts[1]
+            };
+        } else {
+            return {
+                first: input,
+                second: false
+            };
+        }
+    }
+}
+
+export default CommandUtils
