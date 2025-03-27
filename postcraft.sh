62f555a8be3377d2b22b676a8a61546dabeb42d3
Danny Ayers <danny.ayers@gmail.com>
2025-03-27 14:44:53 +0100

    scripts

--- postcraft.sh
+++ postcraft.sh
@@ -1,5 +1,11 @@
+cd ~/hyperdata/transmissions # my local path
 
-./trans md-to-sparqlstore ~/sites/strandz.it/postcraft
-./trans postcraft-statics ~/sites/strandz.it/postcraft
-./trans sparqlstore-to-html ~/sites/strandz.it/postcraft
-./trans sparqlstore-to-site-indexes ~/sites/strandz.it/postcraft
+#./trans md-to-sparqlstore ~/sites/strandz.it/postcraft
+#./trans postcraft-statics ~/sites/strandz.it/postcraft
+#./trans sparqlstore-to-html ~/sites/strandz.it/postcraft
+#./trans sparqlstore-to-site-indexes ~/sites/strandz.it/postcraft
+
+./trans md-to-sparqlstore ~/sites/danny.ayers.name/postcraft
+./trans postcraft-statics ~/sites/danny.ayers.name/postcraft
+./trans sparqlstore-to-html ~/sites/danny.ayers.name/postcraft
+./trans sparqlstore-to-site-indexes ~/sites/danny.ayers.name/postcraft
---
+++ tbox.sh
@@ -0,0 +1,4 @@
+sudo systemctl stop tbox
+cd ~/hyperdata/tbox # my local dir
+docker-compose down
+docker-compose up -d
