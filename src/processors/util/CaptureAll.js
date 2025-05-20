0b1203599c6aa15047f95c5a0e873c479bd8b913
Danny Ayers <danny.ayers@gmail.com>
2025-05-20 14:03:56 +0200

    comments

--- src/processors/util/CaptureAll.js
+++ src/processors/util/CaptureAll.js
@@ -1,6 +1,43 @@
+// src/processors/util/CaptureAll.js
 /// `USE flow/Accumulate.js instead`
 import logger from '../../utils/Logger.js'
 import Processor from '../../model/Processor.js'
+
+/**
+ * @class CaptureAll
+ * @extends Processor
+ * @classdesc
+ * **a Transmissions Processor**
+ *
+ * Deprecated: Use `flow/Accumulate.js` instead.
+ *
+ * Captures all incoming messages into a shared `whiteboard` array. Designed as a singleton to accumulate messages for later processing or inspection.
+ *
+ * ### Processor Signature
+ *
+ * #### __*Settings*__
+ * * **`whiteboard`** - (array) Shared array to accumulate messages; initialized if not present
+ *
+ * #### __*Input*__
+ * * **`message`** - Any message object to be captured
+ *
+ * #### __*Output*__
+ * * **`message`** - Unmodified (but processing is interrupted by an error)
+ *
+ * #### __*Behavior*__
+ * * Captures every message into the `whiteboard` array
+ * * Throws an error to indicate deprecation and halt processing
+ * * Enforces singleton pattern for the processor instance
+ *
+ * #### __*Side Effects*__
+ * * Mutates the `whiteboard` array in config
+ *
+ * #### __*Tests*__
+ * * (Add test references here if available)
+ *
+ * #### __*ToDo*__
+ * * Remove this processor in favor of `flow/Accumulate.js`
+ */
 
 class CaptureAll extends Processor {
     constructor(config) {
